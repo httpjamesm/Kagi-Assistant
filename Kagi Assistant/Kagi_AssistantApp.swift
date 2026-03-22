38235becd00093e481d70d1e85f38de2fd54246c
httpjamesm <github@httpjames.space>
2026-03-22 19:19:59 -0400

    Initial Commit

---
+++ Kagi Assistant/Kagi_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kagi_AssistantApp.swift
+//  Kagi Assistant
+//
+//  Created by James on 2026-03-22.
+//
+
+import SwiftUI
+
+@main
+struct Kagi_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
